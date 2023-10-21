fff56159b70039503198d2e069e84f7a3c4f8d50
LDorantes <leodoor2@gmail.com>
2023-10-20 18:21:22 -0600

    Classes book,library and User added

---
+++ Library/class_book.java
@@ -0,0 +1,13 @@
+public class Libro {
+    private String title;
+    private String author;
+    private String genre;
+    private int availability;
+
+    public Libro (String title, String author, int availability,String genre){
+    this.title=title;
+    this.author=author;
+    this.availability=availability;
+    this.genre=genre;
+    }
+}
